948e55b680f5a42e7b1a1491522e8391ebe448a4
BDadok <bartoszdadok@gmail.com>
2021-03-05 21:57:40 +0100

    add dist

---
+++ dist/assets/js/homework/main.js
@@ -0,0 +1,1 @@
+requirejs.config({paths:{jquery:"vendor/jquery",tweenmax:"vendor/TweenMax.min",async:"components/async",google:"components/google",waypoints:"components/waypoints.min"},shim:{bxslider:{deps:["jquery"]},waypoints:{deps:["jquery"]}}});
